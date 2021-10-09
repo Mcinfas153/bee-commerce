c3757f6c2bae5f2e98b1876f709ca19ce346ce7d
Mc Infas <mcinfas9394@gmail.com>
2021-10-09 13:57:46 +0400

    Login UI changed

--- resources/js/components/Login.js
+++ resources/js/components/Login.js
@@ -4,26 +4,28 @@ export default function Login() {
     return (
         <div className="login">
             <div className="m-auto">
-                <div className="lg:w-6/12 md:mt-4 md:py-20 md:p-20 p-10 py-40 rounded-md mx-auto lg:bg-white">
-                    <img src="../..\images\logo.png" alt="" className="mx-auto"/>
-                    <p className="text-3xl font-medium text-center pb-10 pt-3">LOGIN</p>
-                    <p className="mb-3 text-yellow-400">If you have an account with us, please log in.</p>
+                <div className="md:w-4/12 mt-3 p-10 rounded-md mx-auto bg-white">
+                    <p className="text-3xl text-center mb-10 font-bold text-yellow-400">LOGIN</p>
                     <form>
                         <div className="mb-2">
-                            <label htmlFor="username">Enter Your Email Address:</label>
-                            <input type="email" id="username" className="w-full rounded py-2.5 px-3 mb-1 bg-gray-100"/>
+                            <label htmlFor="username">Email: </label>
+                            <input type="email" id="username" className="w-full rounded py-2.5 px-3 mb-1 bg-gray-100 border-2 border-yellow-300 focus:border-blue-500" placeholder="Enter your email" />
                         </div>
                         <div className="mb-3">
-                            <label htmlFor="password">Enter Your Password:</label>
-                            <input type="password" id="password" className="w-full rounded py-2.5 px-3 mb-1 bg-gray-100"/>
+                            <label htmlFor="password">Password: </label>
+                            <input type="password" id="password" className="w-full rounded py-2.5 px-3 mb-1 bg-gray-100 border-2 border-yellow-300 focus:border-blue-500" placeholder="Enter your password" />
                         </div>
-                        <div className="flex justify-between items-center">
-                            <a href="#"><p className="text-blue-400">Already Have Account?</p></a>
-                            <button type="submit" className="float-right p-2.5 px-5 rounded-md bg-blue-400 text-white">Submit</button>
+                        <div className="grid grid-cols-1 md:grid-cols-2 flex items-center justify-items-center md:justify-items-stretch">
+                            <div className="">
+                                <a href="/register"><p className="text-yellow-400 text-center sm:text-left">Already have an account?</p></a>
+                            </div>
+                            <div className="">
+                                <button type="submit" className="float-right p-2.5 px-5 rounded-md bg-yellow-400 text-white mt-3 md:my-0">Submit</button>
+                            </div>
                         </div>
                     </form>
                 </div>
-            </div>         
-        </div>
+            </div >
+        </div >
     )
 }
