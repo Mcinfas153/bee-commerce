941369f6ea9d7d747526e04e93604b091021d6db
Mc Infas <mcinfas9394@gmail.com>
2021-10-10 11:16:23 +0400

    sample logo added

--- resources/js/components/TopNav.js
+++ resources/js/components/TopNav.js
@@ -3,8 +3,11 @@ import { Link } from 'react-router-dom';
 
 export default function TopNav() {
     return (
-        <div className="bg-red-400 p-3">
+        <div className="bg-green-400 p-3">
             <ul className="flex flex-row gap-4 ">
+                <li>
+                    <img src="../images/first-aid-kit.png" className="w-5" />
+                </li>
                 <li className="">
                     <Link to="/">Home</Link>
                 </li>
