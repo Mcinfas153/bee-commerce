6bd79e1db6cfd04945b33cd27a93872c6cf72e4c
Mc Infas <mcinfas9394@gmail.com>
2021-10-17 12:24:38 +0400

    category box updated

--- resources/js/components/CategoryBox.js
+++ resources/js/components/CategoryBox.js
@@ -8,14 +8,6 @@ export default function CategoryBox() {
                     <p className="tw-text-2xl tw-font-bold tw-text-white">Vaprorizer Charger</p>
                     <button type="button" className="tw-w-auto tw-py-2 tw-px-6 md:tw-px-7 tw-rounded-full tw-bg-blue-900 hover:tw-bg-yellow-500 tw-text-white tw-font-bold tw-mt-10 md:tw-mt-20">Shop Now</button>
                 </div>
-                <div className="tw-px-10 tw-py-10 md:tw-mx-4 tw-mx-5 tw-mb-4 tw-rounded-md tw-bg-gradient-to-r tw-from-blue-200 tw-via-pink-400 tw-to-red-500">
-                    <p className="tw-text-2xl tw-font-bold tw-text-white">Vaprorizer Charger</p>
-                    <button type="button" className="tw-w-auto tw-py-2 tw-px-6 md:tw-px-7 tw-rounded-full tw-bg-blue-900 hover:tw-bg-yellow-500 tw-text-white tw-font-bold tw-mt-10 md:tw-mt-20">Shop Now</button>
-                </div>
-                <div className="tw-px-10 tw-py-10 md:tw-mx-4 tw-mx-5 tw-mb-4 tw-rounded-md tw-bg-gradient-to-r tw-from-blue-200 tw-via-pink-400 tw-to-red-500">
-                    <p className="tw-text-2xl tw-font-bold tw-text-white">Vaprorizer Charger</p>
-                    <button type="button" className="tw-w-auto tw-py-2 tw-px-6 md:tw-px-7 tw-rounded-full tw-bg-blue-900 hover:tw-bg-yellow-500 tw-text-white tw-font-bold tw-mt-10 md:tw-mt-20">Shop Now</button>
-                </div>
             </div>
         </div>
     )
