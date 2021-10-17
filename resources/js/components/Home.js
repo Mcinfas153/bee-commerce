9aabc09b160207eb85148274e2ace34497db125d
Mc Infas <mcinfas9394@gmail.com>
2021-10-17 13:10:01 +0400

    category bar added to home

--- resources/js/components/Home.js
+++ resources/js/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import CategoryBar from './category/CategoryBar'
 import CategoryBox from './category/CategoryBox'
 
 export default function Home() {
     return (
         <div>
-
+            <CategoryBar />
             <div className="tw-grid tw-grid-cols-1 md:tw-grid-cols-3 gap-2 tw-my-8">
                 <div>
                     <CategoryBox name="Electronics" />
