62dab5a2b8b6aeee25ae8494b92593d7c48a5cdd
M.I Ihlas <85578718+ihlas575@users.noreply.github.com>
2021-10-19 12:08:09 +0530

    Small Updates

--- resources/js/components/category/CategoryBar.js
+++ resources/js/components/category/CategoryBar.js
@@ -7,31 +7,31 @@ export default function CategoryBar() {
             <div className="tw-my-8 tw-mx-7 lg:tw-mx-20">
                 <div className="tw-grid tw-grid-cols-3 sm:tw-grid-cols-5 md:tw-grid-cols-6 lg:tw-grid-cols-9 tw-gap-5 tw-mx-auto">
                     <div>
-                        <CategoryRound name="Electronics" />
+                        <CategoryRound name="Trending" />
+                    </div>
+                    <div>
+                        <CategoryRound name="Offers" />
+                    </div>
+                    <div>
+                        <CategoryRound name="Clothes" />
                     </div>
                     <div>
                         <CategoryRound name="Electronics" />
                     </div>
                     <div>
-                        <CategoryRound name="Electronics" />
+                        <CategoryRound name="Furnitures" />
                     </div>
                     <div>
-                        <CategoryRound name="Electronics" />
+                        <CategoryRound name="Men" />
                     </div>
                     <div>
-                        <CategoryRound name="Electronics" />
+                        <CategoryRound name="Women" />
                     </div>
                     <div>
-                        <CategoryRound name="Electronics" />
+                        <CategoryRound name="Mobiles" />
                     </div>
                     <div>
-                        <CategoryRound name="Electronics" />
-                    </div>
-                    <div>
-                        <CategoryRound name="Electronics" />
-                    </div>
-                    <div>
-                        <CategoryRound />
+                        <CategoryRound name="Groceries" />
                     </div>
                 </div>
             </div>
--- resources/js/components/category/CategoryRound.js
+++ resources/js/components/category/CategoryRound.js
@@ -5,7 +5,7 @@ export default function CategoryRound(props) {
     const { name } = props
 
     return (
-        <div className="categoryRound mx-auto">
+        <div className="categoryRound mx-auto tw-transition tw-duration-500 tw-ease-in-out tw-transform hover:tw-translate-y-1 hover:tw-scale-105">
             <img src="../images/Category/electronics.jpg" className="rounded-circle tw-cursor-pointer mx-auto tw-w-20 lg:tw-w-full" />
             <p className="tw-font-bold text-center">{name}</p>
         </div>
