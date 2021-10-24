31b68350d1b0775e6d815bc73ccc97c2fe70ed10
Mc Infas <mcinfas9394@gmail.com>
2021-10-24 11:51:11 +0400

    category test modified

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -13,7 +13,7 @@ class CategoryTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_list_categories()
     {
         $response = $this->get(route('category.index'));
         $response
