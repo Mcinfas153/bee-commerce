41b89ba8d6b3f04178fdc6eddca272899a46d0f9
M.I Ihlas <85578718+ihlas575@users.noreply.github.com>
2021-10-25 18:29:12 +0530

    Small Changes

--- resources/js/components/admin/store/CartPaymentInfo.js
+++ resources/js/components/admin/store/CartPaymentInfo.js
@@ -5,18 +5,18 @@ export default function CartPaymentInfo() {
         <div className="CartPaymentInfo">
             <div className="tw-border rounded p-5">
                 <div className="tw-flex tw-justify-between tw-items-center">
-                    <p>Sub-Total</p>
-                    <p>$ 299</p>
+                    <p className="tw-font-semibold tw-text-blue-900">Sub-Total</p>
+                    <p className="tw-font-bold tw-text-yellow-500 tw-text-base">$ 299</p>
                 </div>
                 <hr className="tw-w-11/12 mx-auto tw-my-5"/>
                 <div className="tw-flex tw-justify-between tw-items-center">
-                    <p>Sub-Total</p>
-                    <p>$ 299</p>
+                    <p className="tw-font-semibold tw-text-blue-900">Shipping</p>
+                    <p className="tw-font-bold tw-text-yellow-500 tw-text-base">$ 29</p>
                 </div>
                 <hr className="tw-w-11/12 mx-auto tw-my-5"/>
                 <div className="tw-flex tw-justify-between tw-items-center">
-                    <p>Sub-Total</p>
-                    <p>$ 299</p>
+                    <p className="tw-font-semibold tw-text-blue-900">Total</p>
+                    <p className="tw-font-bold tw-text-yellow-500 tw-text-base">$ 329</p>
                 </div>
                 <hr className="tw-w-11/12 mx-auto tw-my-5"/>
                 <button className="tw-w-full mt-4 tw-py-3 tw-bg-blue-900 hover:tw-bg-yellow-500 tw-rounded tw-text-white tw-font-bold">🛒 Proceed to Pay</button>
