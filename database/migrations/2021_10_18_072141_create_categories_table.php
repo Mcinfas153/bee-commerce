b0c4832d91cfd1799e241a9a2758ceff08043d90
Mc Infas <mcinfas9394@gmail.com>
2021-10-26 16:17:56 +0400

    image added to category seeder

--- database/migrations/2021_10_18_072141_create_categories_table.php
+++ database/migrations/2021_10_18_072141_create_categories_table.php
@@ -18,6 +18,7 @@ class CreateCategoriesTable extends Migration
             Schema::create('categories', function (Blueprint $table) {
                 $table->id();
                 $table->string('name');
+                $table->string('slug');
                 $table->tinyInteger('is_parent')->default(1);
                 $table->foreignIdFor(Category::class)->default(0);
                 $table->text('image_url')->nullable();
--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -17,25 +18,27 @@ class CategorySeeder extends Seeder
     {
         Category::truncate();
 
+        $faker = Factory::create();
+
         DB::table('categories')->insert([
-            ['name' => 'Animals & Pet Supplies'],
-            ['name' => 'Arts & Entertainment'],
-            ['name' => 'Baby & Toddler'],
-            ['name' => 'Business & Industrial'],
-            ['name' => 'Cameras & Optics'],
-            ['name' => 'Clothing & Accessories'],
-            ['name' => 'Electronics'],
-            ['name' => 'Food, Beverages & Tobacco'],
-            ['name' => 'Furniture'],
-            ['name' => 'Hardware'],
-            ['name' => 'Health & Beauty'],
-            ['name' => 'Home & Garden'],
-            ['name' => 'Media'],
-            ['name' => 'Office Supplies'],
-            ['name' => 'Software'],
-            ['name' => 'Sporting Goods'],
-            ['name' => 'Toys & Games'],
-            ['name' => 'Vehicles & Parts']
+            ['name' => 'Animals & Pet Supplies', 'slug' => '', 'image_url' => $faker->imageUrl(100, 100)],
+            ['name' => 'Arts & Entertainment', 'slug' => '', 'image_url' => $faker->imageUrl(100, 100)],
+            ['name' => 'Baby & Toddler', 'slug' => '', 'image_url' => $faker->imageUrl(100, 100)],
+            ['name' => 'Business & Industrial', 'slug' => '', 'image_url' => $faker->imageUrl(100, 100)],
+            ['name' => 'Cameras & Optics', 'slug' => '', 'image_url' => $faker->imageUrl(100, 100)],
+            ['name' => 'Clothing & Accessories', 'slug' => '', 'image_url' => $faker->imageUrl(100, 100)],
+            ['name' => 'Electronics', 'slug' => '', 'image_url' => $faker->imageUrl(100, 100)],
+            ['name' => 'Food, Beverages & Tobacco', 'slug' => '', 'image_url' => $faker->imageUrl(100, 100)],
+            ['name' => 'Furniture', 'slug' => '', 'image_url' => $faker->imageUrl(100, 100)],
+            ['name' => 'Hardware', 'slug' => '', 'image_url' => $faker->imageUrl(100, 100)],
+            ['name' => 'Health & Beauty', 'slug' => '', 'image_url' => $faker->imageUrl(100, 100)],
+            ['name' => 'Home & Garden', 'slug' => '', 'image_url' => $faker->imageUrl(100, 100)],
+            ['name' => 'Media', 'slug' => '', 'image_url' => $faker->imageUrl(100, 100)],
+            ['name' => 'Office Supplies', 'slug' => '', 'image_url' => $faker->imageUrl(100, 100)],
+            ['name' => 'Software', 'slug' => '', 'image_url' => $faker->imageUrl(100, 100)],
+            ['name' => 'Sporting Goods', 'slug' => '', 'image_url' => $faker->imageUrl(100, 100)],
+            ['name' => 'Toys & Games', 'slug' => '', 'image_url' => $faker->imageUrl(100, 100)],
+            ['name' => 'Vehicles & Parts', 'slug' => '', 'image_url' => $faker->imageUrl(100, 100)]
         ]);
     }
 }
