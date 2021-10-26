c9627ead534d9dab7099ae24f774e0180e889156
M.I Ihlas <85578718+ihlas575@users.noreply.github.com>
2021-10-26 14:19:37 +0530

    Left Bar Updated

--- resources/js/components/admin/account/MyAccount.js
+++ resources/js/components/admin/account/MyAccount.js
@@ -3,8 +3,15 @@ import LeftBar from './LeftBar'
 
 export default function MyAccount() {
     return (
-        <div>
-            <LeftBar />
+        <div className="tw-container tw-mx-auto tw-grid md:tw-grid-cols-12 tw-gap-4">
+            <div className="md:tw-col-span-5 lg:tw-col-span-4">
+                <LeftBar className="tw-fixed tw-top-0" />
+            </div>
+            <div className="md:tw-col-span-7 lg:tw-col-span-8 tw-hidden md:tw-block">
+                <p className="tw-my-10 tw-mx-5">
+                    This is Informations Page
+                </p>
+            </div>
         </div>
     )
 }
