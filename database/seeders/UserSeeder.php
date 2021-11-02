af91218f4962bcd0c37fbc1aab0be2808a4175e4
Mc Infas <mcinfas9394@gmail.com>
2021-11-02 12:33:45 +0400

    user seeder created

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        User::truncate();
+
+        DB::table('users')->insert([
+            'first_name' => 'Infas',
+            'last_name' => 'Mohammed',
+            'email' => 'mcinfas@gmail.com',
+            'password' => Hash::make('12345678')
+        ]);
+    }
+}
