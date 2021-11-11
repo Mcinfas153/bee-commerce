32fd9770c499ed2af87c3fa5cc992547d6fa7144
Mc Infas <mcinfas9394@gmail.com>
2021-11-11 16:33:48 +0400

    product box UI modified

--- resources/js/components/product/ProductBox.js
+++ resources/js/components/product/ProductBox.js
@@ -6,19 +6,21 @@ export default function ProductBox(props) {
     const { id, name, image, price } = props
 
     return (
-        <div className="tw-transition tw-transform tw-duration-500 tw-border-2 tw-rounded tw-border-yellow-500 hover:tw-scale-105 tw-bg-white hover:tw-border-gray-200 tw-p-5 tw-transition tw-duration-200 tw-ease-in-out tw-shadow-sm">
-            <div className="tw-bg-yellow-500 tw-text-blue-900 tw-font-bold tw-text-xs tw-w-12 tw-uppercase tw-px-1 tw-py-1 tw-text-center tw-rounded-md tw-mb-2">new</div>
-            <div className="tw-flex tw-justify-center">
+        <div className="tw-transition tw-transform tw-duration-500 tw-border tw-rounded tw-border-yellow-500 hover:tw-scale-105 tw-bg-white hover:tw-border-gray-200 tw-transition tw-duration-200 tw-ease-in-out tw-shadow-sm">
+            <div className="tw-bg-yellow-500 tw-text-blue-900 tw-font-bold tw-text-xs tw-w-full tw-uppercase tw-px-1 tw-py-1 tw-text-center tw-mb-2">14% Discount</div>
+            <div className="tw-px-2 tw-py-5">
+                <div className="tw-flex tw-justify-center">
+                    <Link to={`/product/${id}`}>
+                        <img loading="lazy" className="tw-transform tw-transition tw-duration-1000 tw-w-40 hover:tw-scale-95" src={image} />
+                    </Link>
+                </div>
                 <Link to={`/product/${id}`}>
-                    <img loading="lazy" className="tw-transform tw-transition tw-duration-1000 tw-w-24 hover:tw-scale-75" src={image} />
+                    <p className="tw-text-center tw-text-yellow-500 hover:tw-text-blue-900 tw-transition tw-duration-500 tw-font-medium tw-mt-3 tw-text-sm">{name}</p>
                 </Link>
-            </div>
-            <Link to={`/product/${id}`}>
-                <p className="tw-text-center tw-text-yellow-500 hover:tw-text-blue-900 tw-transition tw-duration-500 tw-font-medium tw-mt-3 tw-text-sm">{name}</p>
-            </Link>
-            <p className="tw-text-center tw-text-blue-900 hover:tw-text-yellow-500 tw-transition tw-duration-500 tw-font-bold tw-text-md tw-my-2">${price} <span className="tw-text-red-500 tw-font-medium tw-ml-1 tw-line-through tw-text-sm">${price}</span></p>
-            <div className="tw-flex tw-justify-center">
-                <button className="tw-transition tw-duration-500 tw-bg-blue-900 hover:tw-bg-yellow-500 tw-text-yellow-500 hover:tw-text-blue-900 tw-transform hover:tw-scale-110 tw-font-bold tw-px-5 tw-py-1">Add to Cart</button>
+                <p className="tw-text-center tw-text-blue-900 hover:tw-text-yellow-500 tw-transition tw-duration-500 tw-font-bold tw-text-sm tw-my-2">${price} <span className="tw-text-red-500 tw-font-medium tw-ml-1 tw-line-through tw-text-sm">${price}</span></p>
+                <div className="tw-flex tw-justify-center">
+                    <button className="tw-transition tw-duration-500 tw-bg-blue-900 hover:tw-bg-yellow-500 tw-text-yellow-500 hover:tw-text-blue-900 tw-transform hover:tw-scale-110 tw-font-bold tw-px-5 tw-py-1">Add to Cart</button>
+                </div>
             </div>
         </div>
     )
