74669edf383468702a7214ec784df68d1ceca7b5
Mc Infas <mcinfas9394@gmail.com>
2021-12-05 13:48:06 +0400

    dashboard, profile component created

--- resources/js/components/admin/account/MyAccount.js
+++ resources/js/components/admin/account/MyAccount.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Switch, Route, useRouteMatch } from 'react-router-dom'
 import LeftBar from './LeftBar'
+import Dashboard from './Dashboard'
+import Profile from './Profile';
 
 export default function MyAccount() {
+
+    let { path } = useRouteMatch();
+
     return (
         <div className="tw-container tw-mx-auto tw-grid md:tw-grid-cols-12 tw-gap-4">
             <div className="md:tw-col-span-5 lg:tw-col-span-4">
@@ -9,7 +15,14 @@ export default function MyAccount() {
             </div>
             <div className="md:tw-col-span-7 lg:tw-col-span-8 tw-hidden md:tw-block">
                 <p className="tw-my-10 tw-mx-5">
-                    This is Informations Page
+                    <Switch>
+                        <Route exact path={path}>
+                            <Dashboard />
+                        </Route>
+                        <Route path={`${path}/profile`}>
+                            <Profile />
+                        </Route>
+                    </Switch>
                 </p>
             </div>
         </div>
