3efe7afb2140ecc675dcc4f55872d3c5209a55ee
Mc Infas <mcinfas9394@gmail.com>
2021-12-05 16:03:27 +0400

    infobox modified

--- resources/js/components/InfoBox.js
+++ resources/js/components/InfoBox.js
@@ -7,7 +7,7 @@ export default function InfoBox(props) {
     return (
         <div className={`tw-border tw-flex tw-flex-col tw-p-5 tw-bg-${color}-500`}>
             <p className="tw-text-white tw-font-bold tw-text-base tw-uppercase tw-tracking-widest">{text}</p>
-            <p className="tw-text-white tw-font-bold tw-text-2xl">{value}</p>
+            <p className="tw-text-white tw-font-bold tw-text-2xl tw-mt-5">{value}</p>
         </div >
     )
 }
