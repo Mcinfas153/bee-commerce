e4bdecfc63f9e7affdc03fd8661e15f1e6e874d4
M.I Ihlas <85578718+ihlas575@users.noreply.github.com>
2022-04-25 11:10:37 +0530

    Navbar Updated

--- resources/js/components/Navbar.js
+++ resources/js/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
+import { faAward, faBolt, faCaretDown, faCarrot, faFemale, faGift, faHome, faIdBadge, faLeaf, faMale, faPercent, faPercentage, faSpider } from '@fortawesome/free-solid-svg-icons'
 import { Offcanvas,Dropdown } from 'react-bootstrap';
 
 export default function Navbar() {
@@ -13,30 +13,52 @@ export default function Navbar() {
 
     return (
         <>
-            <div className="tw-bg-gray-600 tw-p-3 tw-grid tw-grid-cols-5 tw-gap-10">
-                <div className="tw-flex tw-flex-row tw-justify-between tw-align-items-center tw-col-span-full md:tw-col-span-1  md:tw-border-r tw-border-yellow-500 tw-cursor-pointer" onClick={handleShow}>
-                    <p className="tw-uppercase tw-text-gray-50 tw-text-sm tw-font-bold hover:tw-text-white tw-cursor-pointer">All categories</p>
-                    <span className="tw-px-3"><FontAwesomeIcon icon={faCaretDown} className="tw-text-gray-50 tw-text-2xl tw-font-bold tw--mt-2 hover:tw-text-white tw-cursor-pointer" /></span>
+            <div className="tw-bg-gray-600 tw-p-3 tw-grid tw-grid-cols-6 tw-gap-10">
+                <div className="tw-flex tw-flex-row tw-justify-between tw-align-items-center tw-col-span-full md:tw-col-span-1  md:tw-border-r tw-cursor-pointer" onClick={handleShow}>
+                    <p className="tw-text-gray-50 tw-text-xs tw-font-bold hover:tw-text-white tw-cursor-pointer">All categories</p>
+                    <span className="tw-px-5"><FontAwesomeIcon icon={faCaretDown} className="tw-text-gray-50 tw-text-xl tw-font-bold tw--mt-2 hover:tw-text-white tw-cursor-pointer" /></span>
                 </div>
                 <div className="tw-col-span-full md:tw-col-span-4 tw-hidden md:tw-block">
                     <ul className="tw-flex tw-flex-row tw-gap-10 ">
-                        <li className="tw-uppercase tw-text-white tw-text-sm tw-font-bold hover:tw-text-yellow-500">
-                            <Link to={`/category/1/products`}>Electronics</Link>
+                        <li className="tw-text-white tw-text-xs">
+                            <Link to={`/category/1/products`} className="tw-font-bold hover:tw-text-yellow-300 tw-flex tw-items-center">
+                                <FontAwesomeIcon icon={faBolt} className="tw-text-gray-50 tw-text-md hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" />
+                                <p>Electronics</p>
+                            </Link>
                         </li>
-                        <li className="tw-uppercase tw-text-white tw-text-sm tw-font-bold hover:tw-text-yellow-500">
-                            <Link to={`/category/2/products`}>Men</Link>
+                        <li className="tw-text-white tw-text-xs">
+                            <Link to={`/category/1/products`} className="tw-font-bold hover:tw-text-yellow-300 tw-flex tw-items-center">
+                                <FontAwesomeIcon icon={faMale} className="tw-text-gray-50 tw-text-md hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" />
+                                <p>Men</p>
+                            </Link>
                         </li>
-                        <li className="tw-uppercase tw-text-white tw-text-sm tw-font-bold hover:tw-text-yellow-500">
-                            <Link to={`/category/3/products`}>Women</Link>
+                        <li className="tw-text-white tw-text-xs">
+                            <Link to={`/category/1/products`} className="tw-font-bold hover:tw-text-yellow-300 tw-flex tw-items-center">
+                                <FontAwesomeIcon icon={faFemale} className="tw-text-gray-50 tw-text-md hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" />
+                                <p>Women</p>
+                            </Link>                        
                         </li>
-                        <li className="tw-uppercase tw-text-white tw-text-sm tw-font-bold hover:tw-text-yellow-500">
-                            <Link to={`/category/4/products`}>Home</Link>
+                        <li className="tw-text-white tw-text-xs">
+                            <Link to={`/category/1/products`} className="tw-font-bold hover:tw-text-yellow-300 tw-flex tw-items-center">
+                                <FontAwesomeIcon icon={faLeaf} className="tw-text-gray-50 tw-text-md hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" />
+                                <p>Home</p>
+                            </Link>
                         </li>
-                        <li className="tw-uppercase tw-text-white tw-text-sm tw-font-bold hover:tw-text-yellow-500">
-                            <Link to={`/category/5/products`}>Grocery</Link>
+                        <li className="tw-text-white tw-text-xs">
+                            <Link to={`/category/5/products`} className="tw-font-bold hover:tw-text-yellow-300">Grocery</Link>
                         </li>
-                        <li className="tw-uppercase tw-text-white tw-text-sm tw-font-bold hover:tw-text-yellow-500">
-                            <Link to={`/category/6/products`}>Sports</Link>
+                        <li className="tw-text-white tw-text-xs">
+                            <Link to={`/category/6/products`} className="tw-font-bold hover:tw-text-yellow-300">Sports</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="tw-col-span-full md:tw-col-span-1 tw-hidden md:tw-block">
+                    <ul className="tw-flex tw-flex-row tw-gap-10 tw-float-right tw-px-5">
+                        <li className="tw-text-white tw-text-xs">
+                            <Link to={`/category/1/products`} className="tw-font-bold hover:tw-text-yellow-300 tw-flex tw-items-center">
+                                <FontAwesomeIcon icon={faSpider} className="tw-text-gray-50 tw-text-lg tw-font-semibold hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" />
+                                <p>Special Offers</p>
+                            </Link>
                         </li>
                     </ul>
                 </div>
@@ -44,11 +66,12 @@ export default function Navbar() {
 
             <Offcanvas className="tw-w-80" show={show} onHide={handleClose}>
                 <Offcanvas.Header closeButton>
-                    <Offcanvas.Title className='tw-font-bold tw-text-blue-900'>All Categories</Offcanvas.Title>
+                    <Offcanvas.Title className='tw-font-bold tw-text-gray-600'>All Categories</Offcanvas.Title>
                 </Offcanvas.Header>
-                <Offcanvas.Body className='tw-px-0 tw-py-0'>
-                    <p className='tw-text-lg tw-font-bold'></p>
-                    <ul className="">
+                <hr />
+                <Offcanvas.Body className='tw-px-3 tw-py-3'>
+                    <p className='tw-text-md tw-text-yellow-300'>Nothing to show here any categories. Still in working progress.</p>
+                    {/* <ul className="">
                         <li className="tw-py-3 tw-px-5 tw-text-white tw-bg-yellow-500 tw-border tw-rounded tw-text-sm tw-font-bold hover:tw-text-blue-900">
                             <Link to={`/category/1/products`}>Baby & Toddler</Link>
                         </li>
@@ -81,7 +104,7 @@ export default function Navbar() {
                         <li className="tw-py-3 tw-px-5 tw-text-white tw-bg-yellow-500 tw-border tw-rounded tw-text-sm tw-font-bold hover:tw-text-blue-900">
                             <Link className='tw-flex tw-justify-between tw-items-center' to={`/category/7/products`}>Clothes <img src="https://img.icons8.com/fluency-systems-filled/15/ffffff/chevron-down--v2.png"/></Link>
                         </li>
-                    </ul>
+                    </ul> */}
                 </Offcanvas.Body>
             </Offcanvas>
         </>
--- resources/js/components/product/ProductBox.js
+++ resources/js/components/product/ProductBox.js
@@ -1,42 +1,45 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import truncate from 'truncate'
+import '../styles/product-card.css'
 
 export default function ProductBox(props) {
 
     const { id, name, image, price, discount } = props
 
     return (
-        <div className="tw-rounded-sm tw-mr-2 tw-border tw-border-gray-200 tw-bg-white tw-drop-shadow-2xl">
-            <div className="productlabel tw-font-bold tw-text-xs tw-w-6/12 tw-relative tw-rounded-tl-sm tw-py-1"><p>{discount}% Discount</p></div>
-            <div className="tw-pt-3">
-                <div className="tw-flex tw-justify-center">
-                    <Link to={`/product/${id}`}>
-                        <img loading="lazy" className="tw-w-44" src={image} alt="Product Image" title='Product Image' />
-                    </Link>
-                </div>
-                <Link to={`/product/${id}`}>
-                    <p className="tw-text-center tw-text-gray-600 hover:tw-text-yellow-300 tw-transition tw-duration-500 tw-font-medium tw-mt-3 tw-text-lg tw-px-2">{truncate(name, 20)}</p>
-                </Link>
-                <p className="tw-text-center tw-text-gray-600 tw-transition tw-duration-500 tw-font-bold tw-text-sm tw-my-2">${price} <span className="tw-text-red-500 tw-font-medium tw-ml-1 tw-line-through tw-text-sm">${price}</span></p>
-                <p className="tw-text-center tw-text-gray-600 hover:tw-text-yellow-300 tw-transition tw-duration-500 tw-font-medium tw-text-md tw-px-2">Cosmetics</p>
-                <p className="tw-text-center tw-text-gray-600 tw-font-normal tw-text-xs tw-my-2 tw-px-2">${price} with Subscribe & Save discount</p>
-                <p className="tw-text-center tw-text-gray-600 tw-font-bold tw-text-xs tw-my-2">Ships to Sri lanka</p>
-                <div className="tw-flex tw-justify-between tw-items-center tw-pt-1 tw-px-5">
-                    <button className="tw-bg-gray-600 hover:tw-bg-yellow-500 tw-px-2 tw-py-2 tw-rounded-full tw-transition tw-duration-500 tw-ease-in-out tw-transform hover:tw-scale-110"><img src="https://img.icons8.com/material-outlined/20/ffffff/like--v2.png" className="" title='Add to wishlist'/></button>
-                    <div className="tw-flex tw-justify-center tw-items-center tw-px-2">
-                        <img src="https://img.icons8.com/fluency/20/000000/star.png"/>
-                        <img src="https://img.icons8.com/fluency/20/000000/star.png"/>
-                        <img src="https://img.icons8.com/fluency/20/000000/star.png"/>
-                        <img src="https://img.icons8.com/fluency/20/000000/star.png"/>
-                        <img src="https://img.icons8.com/fluency/20/000000/star.png"/>
-                    </div>
-                    <button className="tw-bg-gray-600 hover:tw-bg-yellow-500 tw-px-2 tw-py-2 tw-rounded-full tw-transition tw-duration-500 tw-ease-in-out tw-transform hover:tw-scale-110"><img src="https://img.icons8.com/material-outlined/20/ffffff/shopping-cart--v1.png" title='Add to cart'/></button>
-                </div>
+        <div>
+            <div className="tw-rounded-sm tw-mr-2 tw-border tw-border-gray-200 tw-bg-white tw-drop-shadow-2xl">
+                <div className="productlabel tw-font-bold tw-text-xs tw-w-6/12 tw-relative tw-rounded-tl-sm tw-py-1"><p>{discount}% Discount</p></div>
                 <div className="tw-pt-3">
+                    <div className="tw-flex tw-justify-center">
+                        <Link to={`/product/${id}`}>
+                            <img loading="lazy" className="tw-w-44" src={image} alt="Product Image" title='Product Image' />
+                        </Link>
+                    </div>
                     <Link to={`/product/${id}`}>
-                        <button className="tw-bg-yellow-400 hover:tw-bg-gray-600 tw-w-full tw-text-white tw-py-2 tw-rounded-bl-sm tw-rounded-br-sm tw-flex tw-justify-center tw-items-center tw-transition tw-duration-500"><img src="https://img.icons8.com/material-outlined/18/ffffff/info.png" className="tw-mr-1" title='View more'/> More Info</button>
+                        <p className="tw-text-center tw-text-gray-600 hover:tw-text-yellow-300 tw-transition tw-duration-500 tw-font-medium tw-mt-3 tw-text-lg tw-px-2">{truncate(name, 20)}</p>
                     </Link>
+                    <p className="tw-text-center tw-text-gray-600 tw-transition tw-duration-500 tw-font-bold tw-text-sm tw-my-2">${price} <span className="tw-text-red-500 tw-font-medium tw-ml-1 tw-line-through tw-text-sm">${price}</span></p>
+                    <p className="tw-text-center tw-text-gray-600 hover:tw-text-yellow-300 tw-transition tw-duration-500 tw-font-medium tw-text-md tw-px-2">Cosmetics</p>
+                    <p className="tw-text-center tw-text-gray-600 tw-font-normal tw-text-xs tw-my-2 tw-px-2">${price} with Subscribe & Save discount</p>
+                    <p className="tw-text-center tw-text-gray-600 tw-font-bold tw-text-xs tw-my-2">Ships to Sri lanka</p>
+                    <div className="tw-flex tw-justify-between tw-items-center tw-pt-1 tw-px-5">
+                        <button className="tw-bg-gray-600 hover:tw-bg-yellow-500 tw-px-2 tw-py-2 tw-rounded-full tw-transition tw-duration-500 tw-ease-in-out tw-transform hover:tw-scale-110"><img src="https://img.icons8.com/material-outlined/20/ffffff/like--v2.png" className="" title='Add to wishlist'/></button>
+                        <div className="tw-flex tw-justify-center tw-items-center tw-px-2">
+                            <img src="https://img.icons8.com/fluency/20/000000/star.png"/>
+                            <img src="https://img.icons8.com/fluency/20/000000/star.png"/>
+                            <img src="https://img.icons8.com/fluency/20/000000/star.png"/>
+                            <img src="https://img.icons8.com/fluency/20/000000/star.png"/>
+                            <img src="https://img.icons8.com/fluency/20/000000/star.png"/>
+                        </div>
+                        <button className="tw-bg-gray-600 hover:tw-bg-yellow-500 tw-px-2 tw-py-2 tw-rounded-full tw-transition tw-duration-500 tw-ease-in-out tw-transform hover:tw-scale-110"><img src="https://img.icons8.com/material-outlined/20/ffffff/shopping-cart--v1.png" title='Add to cart'/></button>
+                    </div>
+                    <div className="tw-pt-3">
+                        <Link to={`/product/${id}`}>
+                            <button className="tw-bg-yellow-400 hover:tw-bg-gray-600 tw-w-full tw-text-white tw-py-2 tw-rounded-bl-sm tw-rounded-br-sm tw-flex tw-justify-center tw-items-center tw-transition tw-duration-500"><img src="https://img.icons8.com/material-outlined/18/ffffff/info.png" className="tw-mr-1" title='View more'/> More Info</button>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
