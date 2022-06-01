28af0bfdd3edeee86be648981fdcc2d7d851e180
M.I Ihlas <85578718+ihlas575@users.noreply.github.com>
2022-06-01 16:49:46 +0530

    Footer Updated

--- resources/js/components/Footer.js
+++ resources/js/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleRight, faEnvelope, faPhoneAlt, faPaperPlane, faExternalLinkAlt, faMicrophoneAlt } from '@fortawesome/free-solid-svg-icons'
+import { faAngleRight, faPhoneAlt, faPaperPlane, faExternalLinkAlt, faMicrophoneAlt } from '@fortawesome/free-solid-svg-icons'
+import { Instagram, Facebook, Phone, Public } from '@material-ui/icons';
 
 export default function Footer() {
 
@@ -13,49 +14,49 @@ export default function Footer() {
     return (
         <div className="footer tw-mt-10 tw-items-center tw-bg-gray-700">
             <div className="tw-grid tw-grid-cols-12 tw-mb-5 md:tw-mb-0 lg:tw-pt-14 lg:tw-pb-10">
-                <div className="tw-col-span-12 sm:tw-col-span-6 md:tw-col-span-6 lg:tw-col-span-2 tw-pb-8 tw-px-10 tw-pt-10 sm:tw-py-10 lg:tw-py-0 sm:tw-mx-auto">
-                    <p className="tw-text-xl md:tw-text-xl tw-font-semibold tw-text-white tw-pb-2">About Us</p>
+                <div className="tw-col-span-12 sm:tw-col-span-6 md:tw-col-span-6 lg:tw-col-span-3 xl:tw-col-span-2 tw-pb-8 tw-px-10 tw-pt-10 sm:tw-py-10 lg:tw-py-0 sm:tw-mx-auto">
+                    <p className="tw-text-xl md:tw-text-xl tw-font-semibold tw-text-white tw-pb-3">About Us</p>
                     <ul>
-                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-1 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Careers</p></a></li>
-                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-1 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Blog</p></a></li>
-                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-1 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> About Amazon</p></a></li>
-                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-1 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Investor Relations</p></a></li>
+                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-2 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Careers</p></a></li>
+                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-2 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Blog</p></a></li>
+                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-2 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> About Amazon</p></a></li>
+                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-2 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Investor Relations</p></a></li>
                     </ul>
                 </div>
-                <div className="tw-col-span-12 sm:tw-col-span-6 md:tw-col-span-6 lg:tw-col-span-3 tw-pb-8 tw-px-10 sm:tw-py-10 lg:tw-py-0 sm:tw-mx-auto">
-                    <p className="tw-text-xl md:tw-text-xl tw-font-semibold tw-text-white tw-pb-2">Payment Products</p>
+                <div className="tw-col-span-12 sm:tw-col-span-6 md:tw-col-span-6 lg:tw-col-span-3 xl:tw-col-span-3 tw-pb-8 tw-px-10 sm:tw-py-10 lg:tw-py-0 sm:tw-mx-auto">
+                    <p className="tw-text-xl md:tw-text-xl tw-font-semibold tw-text-white tw-pb-3">Payment Products</p>
                     <ul>
-                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-1 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Shop with Points</p></a></li>
-                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-1 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Special offers</p></a></li>
-                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-1 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Subscribe our plan</p></a></li>
-                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-1 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Reload your Balance</p></a></li>
+                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-2 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Shop with Points</p></a></li>
+                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-2 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Special offers</p></a></li>
+                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-2 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Subscribe our plan</p></a></li>
+                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-2 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Reload your Balance</p></a></li>
                     </ul>
                 </div>
-                <div className="tw-col-span-12 sm:tw-col-span-6 md:tw-col-span-6 lg:tw-col-span-2 tw-pb-8 tw-px-10 lg:tw-py-0 tw-text-end sm:tw-mx-auto">
-                    <p className="tw-text-xl md:tw-text-xl tw-font-semibold tw-text-white tw-pb-2">Let Help You</p>
+                <div className="tw-col-span-12 sm:tw-col-span-6 md:tw-col-span-6 lg:tw-col-span-3 xl:tw-col-span-2 tw-pb-8 tw-px-10 lg:tw-py-0 tw-text-end sm:tw-mx-auto">
+                    <p className="tw-text-xl md:tw-text-xl tw-font-semibold tw-text-white tw-pb-3">Let Help You</p>
                     <ul>
-                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-1 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Your Account</p></a></li>
-                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-1 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Your Orders</p></a></li>
-                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-1 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Your Wishlist</p></a></li>
-                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-1 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Bee Assistant</p></a></li>
+                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-2 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Your Account</p></a></li>
+                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-2 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Your Orders</p></a></li>
+                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-2 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Your Wishlist</p></a></li>
+                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-2 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Bee Assistant</p></a></li>
                     </ul>
                 </div>
-                <div className="tw-col-span-12 sm:tw-col-span-6 md:tw-col-span-6 lg:tw-col-span-3 tw-pb-8 tw-px-10 lg:tw-py-0 sm:tw-mx-auto">
-                    <p className="tw-text-xl md:tw-text-xl tw-font-semibold tw-text-white tw-pb-2">Make Money with Us</p>
+                <div className="tw-col-span-12 sm:tw-col-span-6 md:tw-col-span-6 lg:tw-col-span-3 xl:tw-col-span-3 tw-pb-8 tw-px-10 lg:tw-py-0 sm:tw-mx-auto">
+                    <p className="tw-text-xl md:tw-text-xl tw-font-semibold tw-text-white tw-pb-3">Profit with Us</p>
                     <ul>
-                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-1 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Sell on Bee Stores</p></a></li>
-                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-1 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Advertise your products</p></a></li>
-                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-1 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Self publish with us</p></a></li>
-                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-1 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Hsot a Bee Hub</p></a></li>
+                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-2 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Sell on Bee Stores</p></a></li>
+                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-2 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Advertise your products</p></a></li>
+                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-2 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Self publish with us</p></a></li>
+                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-2 tw-ml-2"><FontAwesomeIcon icon={faAngleRight} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> Hsot a Bee Hub</p></a></li>
                     </ul>
                 </div>
-                <div className="tw-col-span-12 sm:tw-col-span-12 md:tw-col-span-12 lg:tw-col-span-2 tw-pb-8 tw-px-10 lg:tw-py-0 sm:tw-mx-auto">
-                    <p className="tw-text-xl tw-font-semibold tw-text-white tw-pb-2">Contact Us</p>
+                <div className="tw-col-span-12 sm:tw-col-span-12 md:tw-col-span-12 lg:tw-hidden xl:tw-block xl:tw-col-span-2 tw-pb-8 tw-px-10 lg:tw-py-0 sm:tw-mx-auto">
+                    <p className="tw-text-xl tw-font-semibold tw-text-white tw-pb-3">Contact Us</p>
                     <ul>
-                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-1 tw-ml-2"><FontAwesomeIcon icon={faMicrophoneAlt} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> +94-76-487-8633</p></a></li>
-                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-1 tw-ml-2"><FontAwesomeIcon icon={faPhoneAlt} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> +94-76-487-8633</p></a></li>
-                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-1 tw-ml-2"><FontAwesomeIcon icon={faPaperPlane} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> +94-76-487-8633</p></a></li>
-                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-1 tw-ml-2"><FontAwesomeIcon icon={faExternalLinkAlt} className="tw-text-gray-50 tw-text-sm hover:tw-text-yellow-300 tw-cursor-pointer tw-mr-1" /> beeonline.xyz</p></a></li>
+                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-1 tw-ml-2"><Instagram fontSize='small' /> @beeonline2021</p></a></li>
+                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-1 tw-ml-2"><Facebook fontSize='small' /> @beeonline2021</p></a></li>
+                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-1 tw-ml-2"><Phone fontSize='small' /> +94-76-487-8633</p></a></li>
+                        <li><a href="#"><p className="tw-font-semibold tw-text-xs tw-text-white hover:tw-text-yellow-300 tw-mb-1 tw-ml-2"><Public fontSize='small' /> beeonline.xyz</p></a></li>
                     </ul>
                 </div>                
             </div>
